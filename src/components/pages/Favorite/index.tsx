f69229256359952a2fcf16d824a3565de0134663
Elto Oliveira da Silva <petran.elto@gmail.com>
2023-09-18 11:24:45 -0300

    corrigindo a rota favorites

--- src/components/pages/Favorite/index.tsx
+++ src/components/pages/Favorite/index.tsx
@@ -3,10 +3,12 @@ import { Panel } from '../../shared/Panel';
 import { ICharacter } from '../../shared/dto/character.interface';
 import { apiService } from '../../../services/api.service';
 import { Card } from '../../shared/Card';
+import { Title } from './styles';
 
 export function Favorite() {
 
   const [characters, setCharacters] = useState([] as ICharacter[])
+
 
   useEffect(() => {
     apiService.get<ICharacter[]>('characters/favorites')
@@ -27,18 +29,21 @@ export function Favorite() {
     setCharacters(characters?.slice(0));
   }
   return (
-    <Panel>
-      {
-        characters?.map(
-          character => <Card
-            key={character.id}
-            src={character.image}
-            title={character.name}
-            changeFavorite={(fn) => fnChangeFavorite(character, fn)}
-            isFavorite={character.isFavorite}
-          />
-        )
-      }
-    </Panel>
+    <>
+      <Title>Personagens Favoritos</Title>
+      <Panel>
+        {
+          characters?.map(
+            character => <Card
+              key={character.id}
+              src={character.image}
+              title={character.name}
+              changeFavorite={(fn) => fnChangeFavorite(character, fn)}
+              isFavorite={character.isFavorite}
+            />
+          )
+        }
+      </Panel>
+    </>
   )
 }
--- src/components/struture/Main/index.tsx
+++ src/components/struture/Main/index.tsx
@@ -1,21 +1,20 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { MainContainer } from './styles';
 import { Home } from '../../pages/Home';
 import { Favorite } from '../../pages/Favorite';
 import { Register } from '../../pages/Register';
 import { Login } from '../../pages/Login';
 import { Character } from '../../pages/Character';
-import { ProtectedRoute } from '../ProtectedRoute';
+import { useAuth } from '../AuthProvider/userAuth.tsx';
 
 export function Main() {
 
+  const { authenticated, loading } = useAuth()
   return (
     <MainContainer>
       <Routes>
         <Route Component={Home} path='/' index={true} />
-        <Route path='favorites' element={<ProtectedRoute />}>
-          <Route path='favorite' Component={Favorite} />
-        </Route>
+        <Route path='favorites' Component={() => (authenticated && !loading) ? <Favorite /> : <Navigate to='/' />} />
         <Route Component={Register} path='register' />
         <Route Component={Login} path='login' />
         <Route Component={Character} path='character/:id' />
